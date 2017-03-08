eaef98f2e0ab57696a32abe0ac225a9820439410
Peter Kofler <peter.kofler@code-cop.org>
2017-03-08 14:07:32 +0100

    Format sample unit test.

--- Java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ Java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,13 +1,14 @@
 package com.adaptionsoft.games.trivia;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class SomeTest {
 
-	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(false);
-	}
+    @Test
+    public void true_is_true() {
+        assertTrue(true);
+    }
+
 }
