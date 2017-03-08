a61678c30fcbd8c194b7589fb611b75a2a9f71af
Peter Kofler <peter.kofler@code-cop.org>
2017-03-08 14:06:39 +0100

    Use ApprovalTests with sample test.

---
+++ Java/src/test/java/com/adaptionsoft/games/trivia/SampleArrayTest.java
@@ -0,0 +1,21 @@
+package com.adaptionsoft.games.trivia;
+
+import java.util.Arrays;
+
+import org.approvaltests.Approvals;
+import org.approvaltests.reporters.QuietReporter;
+import org.approvaltests.reporters.UseReporter;
+import org.approvaltests.reporters.macosx.KDiff3Reporter;
+import org.junit.Test;
+
+@UseReporter(value = { KDiff3Reporter.class, QuietReporter.class })
+public class SampleArrayTest {
+
+    @Test
+    public void testList() {
+        String[] names = { "Llewellyn", "James", "Dan", "Jason", "Katrina" };
+        Arrays.sort(names);
+        Approvals.verifyAll("", names);
+    }
+
+}
