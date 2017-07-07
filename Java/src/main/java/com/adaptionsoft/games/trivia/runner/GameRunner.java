36931b6953ea2c47554f196454da54692312a4c9
johan <johan@235.0.10.10.in-addr.arpa>
2017-07-07 13:45:25 +0200

    rename roll let inpenaltybox logic stand out

--- Java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ Java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -24,7 +24,7 @@ public class GameRunner {
 	
 		do {
 			
-			aGame.roll(rand.nextInt(5) + 1);
+			aGame.moveAndAsk(rand.nextInt(5) + 1);
 			
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
--- Java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ Java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -8,84 +8,93 @@ public class Game {
     final Score score = new Score(6);
 
     ArrayList<String> players = new ArrayList<>();
-    boolean[] inPenaltyBox  = new boolean[6];
+    boolean[] inPenaltyBox = new boolean[6];
 
     int currentPlayer = 0;
     boolean isGettingOutOfPenaltyBox;
 
-	public boolean isPlayable() {
-		return (howManyPlayers() >= 2);
-	}
+    public boolean isPlayable() {
+        return (howManyPlayers() >= 2);
+    }
 
-	public boolean add(String playerName) {
-	    players.add(playerName);
-	    inPenaltyBox[howManyPlayers()] = false;
+    public boolean add(String playerName) {
+        players.add(playerName);
 
-	    System.out.println(playerName + " was added");
-	    System.out.println("They are player number " + players.size());
-		return true;
-	}
+        System.out.println(playerName + " was added");
+        System.out.println("They are player number " + players.size());
+        return true;
+    }
 
-	public int howManyPlayers() {
-		return players.size();
-	}
+    public int howManyPlayers() {
+        return players.size();
+    }
 
-	public void roll(int roll) {
-		System.out.println(players.get(currentPlayer) + " is the current player");
-		System.out.println("They have rolled a " + roll);
 
-        if (inPenaltyBox[currentPlayer]) {
-			if (roll % 2 != 0) {
-				isGettingOutOfPenaltyBox = true;
+    public void moveAndAsk(int roll) {
+        boolean isLucky = roll % 2 != 0;
+        Runnable doSomething = () -> askQuestionOfDestinationPlace(roll);
 
-				System.out.println(players.get(currentPlayer) + " is getting out of the penalty box");
+        System.out.println(players.get(currentPlayer) + " is the current player");
+        System.out.println("They have rolled a " + roll);
 
-				String name = players.get(currentPlayer);
-				String currentCategory = move.move(name, currentPlayer, roll);
-				ask.askQuestionFor(currentCategory);
+        handle(players.get(currentPlayer), isLucky, doSomething);
 
-			} else {
-				System.out.println(players.get(currentPlayer) + " is not getting out of the penalty box");
-				isGettingOutOfPenaltyBox = false;
-			}
+    }
 
-		} else {
+    private void handle(String name, boolean isLucky, Runnable doSomething) {
+        if (!inPenaltyBox[currentPlayer]) {
+            doSomething.run();
 
-            String name = players.get(currentPlayer);
-			String currentCategory = move.move(name, currentPlayer, roll);
-			ask.askQuestionFor(currentCategory);
-		}
-	}
+        } else if (isLucky) {
+            System.out.println(name + " is getting out of the penalty box");
+            isGettingOutOfPenaltyBox = true;
+            doSomething.run();
 
-	public boolean wasCorrectlyAnswered() {
-	    final boolean didPlayerNotWin;
-        if (!inPenaltyBox[currentPlayer] || isGettingOutOfPenaltyBox){
+        } else {
+            System.out.println(name + " is not getting out of the penalty box");
+            isGettingOutOfPenaltyBox = false;
+        }
+    }
+
+    private void askQuestionOfDestinationPlace(int roll) {
+        String name = players.get(currentPlayer);
+        String currentCategory = move.move(name, currentPlayer, roll);
+        ask.askQuestionFor(currentCategory);
+    }
+
+    public boolean wasCorrectlyAnswered() {
+        final boolean didPlayerNotWin;
+        if (canAnswerQuestion()) {
             String name = players.get(currentPlayer);
             didPlayerNotWin = score.correctAnswer(name, currentPlayer);
 
-		} else {
+        } else {
             didPlayerNotWin = true;
         }
 
         changePlayer();
 
         return didPlayerNotWin;
-	}
-
-    private void changePlayer() {
-	    currentPlayer = (currentPlayer + 1) % players.size();
     }
 
-    public boolean wrongAnswer(){
-	    String name = players.get(currentPlayer);
+    private boolean canAnswerQuestion() {
+        return !inPenaltyBox[currentPlayer] || isGettingOutOfPenaltyBox;
+    }
+
+    private void changePlayer() {
+        currentPlayer = (currentPlayer + 1) % players.size();
+    }
+
+    public boolean wrongAnswer() {
+        String name = players.get(currentPlayer);
         boolean didPlayerNotWin = score.wrongAnswer(name, currentPlayer);
 
         inPenaltyBox[currentPlayer] = true;
-        System.out.println(name+ " was sent to the penalty box");
+        System.out.println(name + " was sent to the penalty box");
 
         changePlayer();
 
         return didPlayerNotWin;
-	}
+    }
 
 }
