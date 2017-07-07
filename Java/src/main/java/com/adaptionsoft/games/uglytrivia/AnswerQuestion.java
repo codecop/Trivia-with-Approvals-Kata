115c6c0f20cbbf5e7771570850cc4f3cd7f34040
johan <182208+martinsson@users.noreply.github.com>
2017-07-07 13:01:48 +0200

    simplify conditional logic in roll

--- Java/src/main/java/com/adaptionsoft/games/uglytrivia/AnswerQuestion.java
+++ Java/src/main/java/com/adaptionsoft/games/uglytrivia/AnswerQuestion.java
@@ -12,19 +12,17 @@ public class AnswerQuestion {
         System.out.println("Answer was correct!!!!");
         purses[currentPlayer]++;
         System.out.println(name + " now has " + purses[currentPlayer] + " Gold Coins.");
-
         return didPlayerNotWin(currentPlayer);
     }
 
+    public boolean wrong(String name, int currentPlayer) {
+        System.out.println("Question was incorrectly answered");
+        return didPlayerNotWin(currentPlayer);
+    }
+    
     private boolean didPlayerNotWin(int currentPlayer) {
         boolean didPlayerWin = purses[currentPlayer] == 6;
         return !didPlayerWin;
-    }
-
-    boolean wrong(String name, int currentPlayer) {
-        System.out.println("Question was incorrectly answered");
-        System.out.println(name+ " was sent to the penalty box");
-        return didPlayerNotWin(currentPlayer);
     }
 
     // TODO comment on this class
