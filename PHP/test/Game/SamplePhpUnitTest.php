51a6e0add79fd9bf08324b1714bd7038c0ab0ee4
Peter Kofler <peter.kofler@code-cop.org>
2018-06-10 20:13:49 +0200

    Sample test.

---
+++ PHP/test/Game/SamplePhpUnitTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Game;
+
+class SamplePhpUnitTest extends \PHPUnit_Framework_TestCase {
+
+    /** @test */
+    function testAddition() {
+        $this->assertEquals(2, 1 + 1);
+    }
+
+}
