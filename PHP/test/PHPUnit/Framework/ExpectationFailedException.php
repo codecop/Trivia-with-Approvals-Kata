43381a3497bfad23c791c96e97aa2345d9df0ddb
Peter Kofler <peter.kofler@code-cop.org>
2018-06-10 21:51:55 +0200

    Ammend retro PHPUnit, needs a constructor.

--- PHP/test/PHPUnit/Framework/ExpectationFailedException.php
+++ PHP/test/PHPUnit/Framework/ExpectationFailedException.php
@@ -5,4 +5,8 @@ use PHPUnit_Framework_ExpectationFailedException;
 
 class ExpectationFailedException extends PHPUnit_Framework_ExpectationFailedException {
 
+    public function __construct() {
+        parent::__construct("Approvals reporter failed");
+    }
+
 }
