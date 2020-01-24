6cb67a8cdd822d31b9bba990b4db85c77ba16810
Peter Kofler <peter.kofler@code-cop.org>
2020-01-24 16:18:53 +0100

    Rename approval test to avoid conflict with participants' code.

--- Kotlin/src/test/kotlin/com/adaptionsoft/games/trivia/WholeGameTest.kt
+++ Kotlin/src/test/kotlin/com/adaptionsoft/games/trivia/WholeGameTest.kt
@@ -5,31 +5,31 @@ import com.adaptionsoft.games.trivia.runner.main
 import org.approvaltests.Approvals
 import org.junit.Test
 
-class GoldenMasterTest {
+class WholeGameTest {
 
     @Test
-    fun golden_master_seed1() {
+    fun plays_trivia_seed1() {
         val args = arrayOf("93898394832")
         main(args)
         Approvals.verify(GameRunner.logger.logsAsString())
     }
 
     @Test
-    fun golden_master_seed2() {
+    fun plays_trivia_seed2() {
         val args = arrayOf("810938938")
         main(args)
         Approvals.verify(GameRunner.logger.logsAsString())
     }
 
     @Test
-    fun golden_master_seed3() {
+    fun plays_trivia_seed3() {
         val args = arrayOf("8173453")
         main(args)
         Approvals.verify(GameRunner.logger.logsAsString())
     }
 
     @Test
-    fun golden_master_seed4() {
+    fun plays_trivia_seed4() {
         val args = arrayOf("76298317238")
         main(args)
         Approvals.verify(GameRunner.logger.logsAsString())
